b9ed19246c0421daaa9be0b521bf5bc4a44f49a9
karthikmallina <mallinakarthik7298@gmail.com>
2025-11-07 20:21:56 +0530

    session-06

--- roboshop.sh
+++ roboshop.sh
@@ -7,20 +7,21 @@ DOMAIN_NAME="dawsk86s.fun"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0da2b8c8343aa6f01 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instance[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
+    # Get Private IP
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids i-0d32b66ecd303e445 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.dawsk86s.fun
     else
-        IP=$(aws ec2 describe-instances --instance-ids i-0d32b66ecd303e445 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         RECORD_NAME="$DOMAIN_NAME" #dawsk86s.fun
     fi
 
     echo "$instance: $IP"
 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $z \
+    --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
         "Comment": "Updating record set"
